60ba344551d00d4865d135b66072dd83223357b7
danymukesha <danymukesha@gmail.com>
2025-12-16 15:45:50 +0100

    fix undocumented calculateMetrics

--- R/classes.R
+++ R/classes.R
@@ -70,6 +70,21 @@ setClass("ScholarData",
     )
 )
 
+#' @name calculateMetrics
+#' @title Calculate Citation Metrics
+#' @description Calculates various citation metrics (e.g., h-index, i10-index, m-index) for a scholar's profile.
+#'
+#' @param object An object of class `ScholarProfile`.
+#' @param metric A character string specifying the metric to calculate. Options are: "h_index", "i10_index", or "m_index".
+#'
+#' @return A numeric value representing the chosen citation metric.
+#'
+#' @examples
+#' scholar_profile <- new("ScholarProfile", publications = data.frame(citedby = c(10, 20, 30, 40), year = c(2000, 2005, 2010, 2015)))
+#' calculateMetrics(scholar_profile, metric = "h_index")
+#' calculateMetrics(scholar_profile, metric = "i10_index")
+#' calculateMetrics(scholar_profile, metric = "m_index")
+#'
 #' @export
 setGeneric("calculateMetrics", function(object, ...) standardGeneric("calculateMetrics"))
 
