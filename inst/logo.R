0b31b6384224991287adf92c49d6fcaa52849c0e
danymukesha <danymukesha@gmail.com>
2025-12-16 15:38:10 +0100

    fix bugs with actions

--- inst/logo.R
+++ inst/logo.R
@@ -1,16 +1,16 @@
 library(hexSticker)
 imgurl <-
-    system.file("man/figures/scholar - hex.png", package = "CiteAnalyzer")
+    system.file("man/figures/scholar.png", package = "CiteAnalyzer")
 sticker(
     imgurl,
     package = "CiteAnalyzer",
     p_size = 12,
     s_x = 1,
-    s_y = .876,
-    s_width = .554,
+    s_y = .8,
+    s_width = .43,
     s_height = .8,
     p_color = "black",
-    h_fill = "white",
-    h_color = "grey",
+    h_fill = "grey",
+    h_color = "white",
     filename = "man/figures/logo.png"
 )
